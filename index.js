90d8292b11b7e293d1bcf333c57adb11590aa813
daipaw0005 <34964252+daipaw0005@users.noreply.github.com>
2017-12-31 19:04:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function kiusi(){
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   const page = await browser.newPage();
-  await page.goto('https://cnhv.co/zbf1'); 
+  await page.goto('https://cnhv.co/zbgc'); 
   console.log('----XONG0--------');
-  setTimeout(async () => await await page.close(), 67000);
-  setTimeout(async () => await browser.close(), 75000);
+  setTimeout(async () => await await page.close(), 57000);
+  setTimeout(async () => await browser.close(), 65000);
   console.log('----XONG--------');
   
   })();
@@ -43,7 +43,7 @@ function f() {
     console.log('-111111111111-------trong f -----------');
     kiusi();
     console.log(Date()); 
-    setTimeout( f, 120000 ); 
+    setTimeout( f, 90000 ); 
     console.log('--tttttttttt--') 
 }
 
